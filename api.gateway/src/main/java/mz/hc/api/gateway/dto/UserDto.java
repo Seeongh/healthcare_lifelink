fc750bfc7787502bcc8a881e5dcf1439db05791a
안성현 <mikstic7@gmail.com>
2024-02-04 23:22:11 +0900

    <feat>api.gateway - gateway dto 커밋

---
+++ api.gateway/src/main/java/mz/hc/api/gateway/dto/UserDto.java
@@ -0,0 +1,17 @@
+package mz.hc.api.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+	private String userId;
+	private String userRole;
+}
