d608d23a760e6abb19b64ee9feec6873b54f5e1b
안성현 <mikstic7@gmail.com>
2024-02-14 23:54:35 +0900

    <refactor>service.commu - community entity 와 user entity 연관관계 매핑

--- service.commu/src/main/java/mz/hc/service/domain/Community.java
+++ service.commu/src/main/java/mz/hc/service/domain/Community.java
@@ -1,7 +1,6 @@
 package mz.hc.service.domain;
 
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -11,7 +10,7 @@ import java.util.Date;
 @Table(name = "health_community")
 public class Community {
 
-    @Id @GeneratedValue //기본키에 매핑
+    @Id @GeneratedValue //기본키에 매핑, @Access (AccessType.FIELD) 생략됨
     @Column(name = "communityId")
     private Long commuId;
 
@@ -19,12 +18,15 @@ public class Community {
     @Lob //대용량 데이터
     private String content;
 
+    @Temporal(TemporalType.TIMESTAMP) //날짜 시간
     private Date regDate;
 
     //내장타입
+
+    @Enumerated(EnumType.STRING) //enum 을 String으로 저장
     private DiseaseCategory category;
 
-    @ManyToOne(fetch= FetchType.EAGER) //기본전략
+    @ManyToOne
     @JoinColumn(name="userId") //실제디비엔 user_id 칼럼명으로 들어감
     private User user;
     // DB는 오브젝트를 저장 할 수 없다.FK는 자바는 오브젝트를 저장할 수 없다.
--- service.commu/src/main/java/mz/hc/service/domain/User.java
+++ service.commu/src/main/java/mz/hc/service/domain/User.java
@@ -2,8 +2,13 @@ package mz.hc.service.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 @Data
 public class User {
+    @Id
+    @Column(name = "userId")
     Long userId;
     String userNm;
 }
